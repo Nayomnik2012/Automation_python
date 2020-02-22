793e0d14158c04a9396de1ba8da38abd73f8cdf2
Nayomnik2012 <a.kovalskiiy@gmail.com>
2020-02-22 15:37:04 +0200

    Class_WORK_4. @elpiankova

---
+++ Lessons_4_Class_Work.py
@@ -0,0 +1,12 @@
+a = ['wqwq', '21', '1234', 'd']
+b = 0
+for i in a:
+    if len(i) % 2 != 0:
+        b += 1
+print(b)
+
+a = {"VALUE" : 1212, "DEAD": 32323, "dsad": "32321"}
+
+for i, value in a.items():
+    print(i, type(value))
+
