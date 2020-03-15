e711e1dd37b8cf70ee68b9cff0516c1a15cae0b2
Nayomnik2012 <a.kovalskiiy@gmail.com>
2020-03-15 20:03:42 +0200

    Homt work 6

--- Lessons_6_Home_Work.py
+++ Lessons_6_Home_Work.py
@@ -3,3 +3,24 @@
 # Создаём один скрипт:
 # Создаём книгу POST /books/, вы запоминаете его id.
 import requests
+
+base_url = "http://pulse-rest-testing.herokuapp.com/"
+data_params = {
+    "title": "Изучения Python Automation",
+    "author": "Александр Ковальский"
+}
+post_api = requests.post(base_url + "books/", data=data_params)
+reg_dict_post = post_api.json()
+id_post = str(reg_dict_post['id'])
+
+# Проверяете, что она создалась и доступна по ссылке GET/books/[id]
+get_api = requests.get(base_url + "books/" + id_post)
+reg_dict_get = get_api.json()
+assert reg_dict_post == reg_dict_get
+
+# Проверяете, что она есть в списке книг по запросу GET /books/
+get_all_books = requests.get(base_url + "books/")
+reg_dict_get = get_all_books.json()
+for i in reg_dict_get:
+    for a, k in i.items():
+        print(k == id_post)
