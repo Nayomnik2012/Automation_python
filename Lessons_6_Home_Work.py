e05083c3e061122b252deda64a25c539d7a79d2c
Ковальський Олександр Володимирович <AVKovalskiy@alfa.bank.int>
2020-03-16 16:34:50 +0200

    Home work 6

--- Lessons_6_Home_Work.py
+++ Lessons_6_Home_Work.py
@@ -11,11 +11,11 @@ data_params = {
 }
 post_api = requests.post(base_url + "books/", data=data_params)
 reg_dict_post = post_api.json()
-id_post = str(reg_dict_post['id'])
+id_post = reg_dict_post['id']
 print(type(id_post))
 
 # Проверяете, что она создалась и доступна по ссылке GET/books/[id]
-get_api = requests.get(base_url + "books/" + id_post)
+get_api = requests.get(base_url + "books/" + str(id_post))
 reg_dict_get = get_api.json()
 assert reg_dict_post == reg_dict_get
 
@@ -24,5 +24,22 @@ get_all_books = requests.get(base_url + "books/")
 reg_dict_get = get_all_books.json()
 for i in reg_dict_get:
     for a, k in i.items():
-        if k == int(id_post):
-            assert k == int(id_post)
+        if k == id_post:
+            print("Книга есть в наличии!")
+
+
+# # Измените данные этой книги методом PUT/books/[id]/
+data_params = {
+    "title": "Изменение книги Python",
+    "author": "Alex Kovalskiy"
+}
+put_api = requests.put(base_url + "books/" + str(id_post), data=data_params)
+
+
+
+# Проверьте, что она изменилась и доступна по ссылке /books/[id]/
+get_api = requests.get(base_url + "books/" + str(id_post))
+reg_dict_get = get_api.json()
+reg_dict_get.pop('id')
+assert data_params == reg_dict_get, "Измененныи данные не совпадают с полученными!"
+
