d22ba6835547f811ce7bb24e9bd7bdd74e3f3932
Nayomnik2012 <a.kovalskiiy@gmail.com>
2020-03-29 20:48:38 +0300

    Personal files HW 6

--- Testings.py
+++ Testings.py
@@ -18,8 +18,8 @@ with open(file, 'r', encoding="utf-8") as new_file:
             d[keys[i]] = values[i]
         emps.append(d)  # <- заполняем его
 deps = [emp["Department"] for emp in emps]
-print(len(list(set(deps))))
-deps = max([emp["salary"] for emp in emps]) # Максимальная ЗП с генератором списка
+print(len(list(set(deps))), ' Количество отделов')
+deps = max([emp["salary"] for emp in emps]) # Максимальная ЗП среди отделов с генератором списка
 
 max_level = 0 # Перебор в ручную, максимальной ЗП
 for d in emps:
@@ -30,7 +30,6 @@ department = [emp['Department'] for emp in emps]
 unigue_department = list(set(department))
 
 max_salary_in_department = dict.fromkeys(unigue_department, 0) # создается словарь с значением 0
-print(max_salary_in_department)
 # max_salary_in_department = {} ## Создаем словарь с нулевыми значениями в ручную
 # for item in unigue_department:
 #     max_salary_in_department[item] = 0
@@ -39,11 +38,24 @@ print(max_salary_in_department)
 for dep in emps: #
     if int(dep['salary']) > int(max_salary_in_department[dep['Department']]):
         max_salary_in_department[dep['Department']] = int(dep['salary'])
-print(max_salary_in_department)
 
 info_max_salary = dict.fromkeys(keys, 0)
-print(info_max_salary)
-for salary in emps:
+for salary in emps: #
     if int(salary['salary']) > info_max_salary['salary']:
+        info_max_salary['Last_name'] = salary['Last_name']
+        info_max_salary['Name'] = salary['Name']
+        info_max_salary['Department'] = salary['Department']
+        info_max_salary['salary'] = int(salary['salary'])
 
-print(info_max_salary)
+def read_file(file):
+    with open(file, 'r') as new_file:
+        first_line = new_file.readline()
+        return first_line
+
+info_max_salary = [i for i in info_max_salary.values()]
+new_sring = ' '.join(str(x) for x in info_max_salary).replace(' ', ';') + ';'
+
+with open("files/text.csv", 'w', encoding="utf-8") as new_file:
+    new_file.write(read_file("files/test.csv"))
+    new_file.write(new_sring)
+
