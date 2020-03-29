635f9c2a4368e7395cc3b29c655d61e6855b5aa2
Nayomnik2012 <a.kovalskiiy@gmail.com>
2020-03-29 13:05:19 +0300

    HW6

--- Lessons_6_Home_Work.py
+++ Lessons_6_Home_Work.py
@@ -175,7 +175,25 @@ with open(file, 'r', encoding="utf-8") as new_file:
         emps.append(d)  # <- заполняем его
 deps = [emp["Department"] for emp in emps]
 print(len(list(set(deps))))
-deps = [emp["salary"] for emp in emps]
-print(max(deps))
-for i in emps:
-    print(i.values())
+deps = max([emp["salary"] for emp in emps]) # Максимальная ЗП с генератором списка
+
+max_level = 0 # Перебор в ручную, максимальной ЗП
+for d in emps:
+    if int(d['salary']) > int(max_level):
+        max_level = d['salary']
+
+department = [emp['Department'] for emp in emps]
+unigue_department = list(set(department))
+
+max_salary_in_department = dict.fromkeys(unigue_department, 0) # создается словарь с значением 0
+print(max_salary_in_department)
+# max_salary_in_department = {} ## Создаем словарь с нулевыми значениями в ручную
+# for item in unigue_department:
+#     max_salary_in_department[item] = 0
+# print(max_salary_in_department)
+
+for dep in emps: #
+    if int(dep['salary']) > int(max_salary_in_department[dep['Department']]):
+        max_salary_in_department[dep['Department']] = int(dep['salary'])
+print(max_salary_in_department)
+
